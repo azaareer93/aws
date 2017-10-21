f62fb8b40b82b5a3150370613c328e2251ea1960
Abood rapper Zaareer <abdullah.zaareer93@gmail.com>
2017-10-21 20:22:07 +0300

    item table

--- public/app/controllers/OrderCtrl.js
+++ public/app/controllers/OrderCtrl.js
@@ -1,14 +1,22 @@
 angular.module('OrderController', ['AuthServices', 'queryService'])
   .controller('OrderCtrl', function($scope, Auth, qService) {
+    $scope.order = {}
+    $scope.order.items=[]
+    $scope.addItem = function (item) {
+      $scope.order.items.push(item)
+      $scope.item = null
+      console.log($scope.order.items);
+    }
+
     $scope.saveOrder = function(order) {
       console.log(order);
-      qService.query("POST", "/api/orders/", order).then(function(data) {
-        if (data.data.order) {
-          console.log(data.data.order);
-        }
-      }).catch(function(err) {
-        console.log(err);
-      });
+      // qService.query("POST", "/api/orders/", order).then(function(data) {
+      //   if (data.data.order) {
+      //     console.log(data.data.order);
+      //   }
+      // }).catch(function(err) {
+      //   console.log(err);
+      // });
     }
 
   });
